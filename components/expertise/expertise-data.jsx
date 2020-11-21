bd0759598bc3421cf5c30e5087ec865b035eabce
LowPulsage <smetiyk@gmail.com>
2020-11-21 23:16:19 +0200

    expertise left styles

---
+++ components/expertise/expertise-data.jsx
@@ -0,0 +1,24 @@
+export const imgData = [
+  {
+    title: 'Web Design & Development',
+    text: 'From web applications to eCommerce website or SaaS platforms, we do it all. Web design and development is our forte and we proudly say we are the best.',
+    img: ''
+  },
+  {
+    title: 'Mobile App Development',
+    text: 'We build apps that people love. We make it easy to put your business at your customers’ fingertips with our intuitive and top-ranked mobile app solutions.',
+    img: ''
+  },
+  {
+    title: 'Custom Software Development',
+    text: 'Not every business is the same, neither their requirements. Our proficient development team will devise the best on-demand custom solutions for your business.',
+    img: ''
+  },
+  {
+    title: 'UI/UX Design',
+    text: 'A subtle yet beautiful design offers an ingenious experience. Designing impeccable user experiences for different devices and screen types and sizes is our forte.',
+    img: ''
+  }
+]
+
+export const skillsData = ["Bootstrap", "HTML", "CSS", "Gulp", "JQuery", "SASS", "Node.JS", "MongoDB", "React", "Next.js", "PHP", "JavaScript"]
--- components/expertise/expertise.jsx
+++ components/expertise/expertise.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
+import Button from '../_elements/button'
+import { imgData, skillsData } from './expertise-data'
 
 const ExpertisePage = () => {
   return (
     <div className="Expertise-page">
-      <div>OUR EXPERTISE</div>
+      <div className="Expertise-page__img-text">OUR EXPERTISE</div>
       <div className="Expertise-page__black-cover">
         <div className="Contact-page__img"></div>
       </div>
+      <span>What We Do</span>
+      <br />
+      <span>
+        We help startups and corporates develop world-class SaaS products: IOT,
+        AI, Risks Assessment, HR/Membership, Marketing
+      </span>
+      {imgData.map((el) => (
+        <div>
+          {el.title}
+          {el.text}
+        </div>
+      ))}
+      {skillsData.map((el) => (
+        <div>{el}</div>
+      ))}
+      <div>WORKING WITH US</div>
+      <div>
+        We strive to meet your business goals through technology implementation.
+      </div>
+      <Button name="GET IN TOUCH" />
     </div>
   )
 }
