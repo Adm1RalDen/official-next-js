8652d73c9bb08addbf8d6fcce33e3105e5da96b4
andrew01011997 <andrew01.01.1997@gmail.com>
2020-12-30 13:43:03 +0200

    adaptation expertise page and added animation

--- components/expertise/expertise.tsx
+++ components/expertise/expertise.tsx
@@ -8,7 +8,7 @@ const ExpertisePage = () => {
     <div className="Expertise-page">
       <div className="Expertise-page__wrapper">
         <div className="Expertise-page__img-text">OUR EXPERTISE</div>
-        <div className="Expertise-page__black-cover"></div>
+        <div className="Expertise-page__black-cover" />
       </div>
       <div className="Expertise-page__top-text-wrapper">
         <span className="Expertise-page__ww-do">What We Do</span>
@@ -18,21 +18,11 @@ const ExpertisePage = () => {
         </span>
       </div>
       <div className="Expertise-page__cards">
-        {imgData.map((el) => (
-          <div className="Expertise-page__cards-each" key={el.title}>
-            <div className="Expertise-page__svg">
-              <img src={el.img} alt="icn" />
-            </div>
-            <div className="Expertise-page__tt-wrapper">
-              <div className="Expertise-page__title">{el.title}</div>
-              <div className="Expertise-page__text">{el.text}</div>
-            </div>
-          </div>
+        {imgData.map((el, i) => (
+          <ImageComponent {...el} key={i} />
         ))}
       </div>
-      <div className="animation-test">
-        <Skills />
-      </div>
+      <Skills />
       <div className="Expertise-page__last-block">
         <div className="Expertise-page__last-block-wws">WORKING WITH US</div>
         <div className="Expertise-page__last-block-text">
@@ -46,6 +36,24 @@ const ExpertisePage = () => {
 }
 
 export default ExpertisePage
+
+interface ImageComponentProps {
+  title: string
+  img: string
+  text: string
+}
+
+const ImageComponent = ({ title, img, text }: ImageComponentProps) => (
+  <div className="Expertise-page__cards-each" key={title}>
+    <div className="Expertise-page__svg">
+      <img src={img} alt="icn" />
+    </div>
+    <div className="Expertise-page__tt-wrapper">
+      <div className="Expertise-page__title">{title}</div>
+      <div className="Expertise-page__text">{text}</div>
+    </div>
+  </div>
+)
 
 // {/* <div className="Expertise-page__knowledges">
 //         {/* <DynamicComponentWithNoSSR/> */}
--- components/expertise/skills.tsx
+++ components/expertise/skills.tsx
@@ -1,17 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import handleViewport from 'react-in-viewport'
 import { skillsData } from './expertise-data'
 
 const Skills = () => {
   return (
-    <div className="Expertise-page__knowledges">
-    {skillsData.map((el, i) => (
-      <div className="Expertise-page__knowledges-wrapper" key={i}>
-        <img src={el.img} alt="knw" />
-        <div className="Expertise-page__knowledges-each">{el.value}</div>
-      </div>
-    ))}
-  </div>
+    <div className="Expertise-page__knowledge">
+      {skillsData.map((el, i) => (
+        <SkillElement
+          {...el}
+          key={i}
+          // onEnterViewport={() => console.log('enter')}
+          // onLeaveViewport={() => console.log('leave')}
+        />
+      ))}
+    </div>
   )
 }
+
+interface ISkillElementProps {
+  img: string
+  name: string
+  inViewport: boolean
+  forwardedRef: React.RefObject<HTMLDivElement>
+  enterCount: number
+}
+
+const SkillElement = handleViewport(
+  ({ img, name, inViewport, forwardedRef, enterCount }: ISkillElementProps) => {
+    const [needRender, setNeedsRender] = useState(false)
+
+    const className = [
+      'Expertise-page__knowledge-wrapper',
+      'Expertise-page__knowledge-wrapper-animated',
+    ]
+
+    useEffect(() => {
+      if (inViewport && !needRender) {
+        setNeedsRender(true)
+      }
+    }, [inViewport, enterCount])
+
+    return (
+      <div ref={forwardedRef} className="Expertise-page__knowledge-container">
+        {needRender ? (
+          <div className={className.join(' ')}>
+            <img src={img} alt="knw" />
+            <div className="Expertise-page__knowledge-each">{name}</div>
+          </div>
+        ) : null}
+      </div>
+    )
+  },
+  {},
+  {}
+)
 
 export default Skills
