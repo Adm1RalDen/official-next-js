a54ebd3e631dfd7455bef4cf0a3e22f9950a6f8a
Adm1RalDen <games.zadrotina@gmail.com>
2021-12-08 13:58:03 +0200

    added eslint rules

--- pages/works/7option/data.ts
+++ pages/works/7option/data.ts
@@ -1,13 +1,29 @@
-import { ProjectProps } from "@/components/works/project/project";
+import { ProjectProps } from '@/components/works/project/project'
 
 export const ProjectData: ProjectProps = {
-  technologies: ['React', 'Redux', 'Redux-Saga', 'Sass','CanvasJS','NodeJS','Express', 'Braintree', 'Amazon Web Service', 'Paypal','Skrill','Perfect Money','MasterCard'],
-  title: "7option",
+  technologies: [
+    'React',
+    'Redux',
+    'Redux-Saga',
+    'Sass',
+    'CanvasJS',
+    'NodeJS',
+    'Express',
+    'Braintree',
+    'Amazon Web Service',
+    'Paypal',
+    'Skrill',
+    'Perfect Money',
+    'MasterCard',
+  ],
+  title: '7option',
   hrefWeb: 'https://7option.com/en/',
-  aimProject: "The 7Option was for created Improve your trading skills and discover a wide range of financial opportunities.",
-  description: "The 7Option was developed as a playground, users will be able to create their own account, calculate the direction of increase or decrease of the schedule, place a bet and, depending on this, win or lose.",
-  image: "../images/7option/large.png",
-  imageAlt: "7option",
-  duration: "10 Months",
-  efforts: "16 Person-months",
+  aimProject:
+    'The 7Option was for created Improve your trading skills and discover a wide range of financial opportunities.',
+  description:
+    'The 7Option was developed as a playground, users will be able to create their own account, calculate the direction of increase or decrease of the schedule, place a bet and, depending on this, win or lose.',
+  image: '../images/7option/large.png',
+  imageAlt: '7option',
+  duration: '10 Months',
+  efforts: '16 Person-months',
 }
